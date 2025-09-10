7fd549192ec5858096e08e45f25833cbd5a39d88
duwjd <2000jungwon@naver.com>
2025-09-11 00:28:11 +0900

    로그 설정

--- 1_02/gpt_basic.py
+++ 1_02/gpt_basic.py
@@ -1,8 +1,17 @@
-from openai import openai
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from openai import OpenAI
 from config.const import OPENAI_API_KEY
+from config.log_config import *
 from prompts.prompt_1_02 import gpt_basic_prompt
 
-client = OpenAI(OPENAI_API_KEY)
+import logging
+
+logger = logging.getLogger("app")
+
+client = OpenAI(api_key= OPENAI_API_KEY)
 prompt = gpt_basic_prompt()
 response = client.chat.completions.create(
     model="gpt-4o",
@@ -10,7 +19,15 @@ response = client.chat.completions.create(
     messages = [
         {
             "role" : "system",
-            "content" : prompt
+            "content" : prompt["system_instruction"]
+        },
+        {
+            "role" : "user",
+            "content" : prompt["prompt"]
         }
     ]
 )
+
+logger.info(response)
+logger.info("-----------------")
+logger.info(response.choices[0].message.content) #response의 내용만 출력함
